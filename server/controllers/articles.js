8b7217523120509efe18da4e0c9211b2e2b82587
Peter A. Higgins <pahiggins@cenabo.com>
2019-01-28 13:40:34 +0000

    Refactor getArticles controller

--- server/controllers/articles.js
+++ server/controllers/articles.js
@@ -22,7 +22,7 @@ exports.getArticles = (req, res, next) => {
     .limit(limit)
     .offset((p - 1) * limit)
     .count('comments.article_id AS comment_count')
-    .orderBy((`articles.${sort_by}`), sort_ascending ? 'asc' : 'desc')
+    .orderBy(`${sort_by}`, sort_ascending ? 'asc' : 'desc')
     .from('comments')
     .leftJoin('articles', 'articles.article_id', '=', 'comments.article_id')
     .groupBy(
